7179e949df3b42d6ec73f6abe0ccd361d48adab6
Rhett Nysko <technicalhatchet@gmail.com>
2026-04-20 16:47:28 -0400

    Fix UI component imports in index.js

--- frontend/components/ui/index.js
+++ frontend/components/ui/index.js
@@ -1,9 +1,10 @@
 // UI Component exports
 export { default as Button } from './Button';
-export { default as Input } from './Input';
-export { default as Select } from './Select';
 export { default as Table } from './Table';
 export { default as Pagination } from './Pagination';
-export { default as Spinner } from './Spinner';
+export { default as Spinner } from './LoadingSpinner';
 export { default as Modal } from './Modal';
-export { default as Alert } from './Alert'; 
+export { default as Alert } from './ErrorAlert';
+export { default as DeleteModal } from './DeleteModal';
+export { default as StatusBadge } from './StatusBadge';
+export { default as Tabs } from './Tabs';
