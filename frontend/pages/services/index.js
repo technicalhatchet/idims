77f5f73fc3baf53e96558b953445b23dbe0049ed
Rhett Nysko <technicalhatchet@gmail.com>
2026-04-20 17:04:23 -0400

    Fix Layout import paths for services page

--- frontend/pages/services/index.js
+++ frontend/pages/services/index.js
@@ -1,22 +1,22 @@
 import React from 'react';
 import Head from 'next/head';
-import Layout from '../../components/layout/Layout';
+import DashboardLayout from '../../components/layouts/DashboardLayout';
 import ServiceList from '../../components/services/ServiceList';
 import { withAuthServerSideProps } from '../../utils/auth';
 
 const ServicesPage = () => {
   return (
-    <Layout>
+    <DashboardLayout>
       <Head>
         <title>Services | IDIMS</title>
       </Head>
       <div className="container mx-auto px-4 py-6">
         <ServiceList />
       </div>
-    </Layout>
+    </DashboardLayout>
   );
 };
 
 export const getServerSideProps = withAuthServerSideProps({ requireAdmin: false });
 
-export default ServicesPage; 
+export default ServicesPage;
